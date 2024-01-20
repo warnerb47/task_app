fc12a58cbefb3a5d3b0ac64acd9dbd03ee23bae5
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-20 12:09:20 +0000

    fix: fix imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TaskFormComponent } from './components/task-form/task-form.component';
-import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TaskFormComponent, TaskComponent],
+  imports: [CommonModule, RouterOutlet, TaskFormComponent, TaskListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
