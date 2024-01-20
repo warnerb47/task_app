eb80bc2b69bd53a4710161ce882d5f10fbbe0824
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-20 13:36:53 +0000

    feat: add updateStatus

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
+import { Observable, of } from 'rxjs';
+import { Todo } from '../../core/interfaces/todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [ TaskComponent ],
+  imports: [ CommonModule, TaskComponent ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
+
+  todos: Todo[] = [
+    { done: true, text: 'Install Dependencies' },
+    { done: false, text: 'Product Feature' },
+    { done: false, text: 'Write Unit test' },
+    { done: false, text: 'Deploy app' },
+  ];
+  todos$: Observable<Todo[]> = of(this.todos);
+
+  loadTodos(): void {
+    this.todos$ = this.fetchTodos();
+  }
+
+  fetchTodos(): Observable<Todo[]> {
+    return of([]);
+  }
+
 
 }
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from '../../core/interfaces/todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss'
 })
 export class TaskComponent {
-
+  
+  @Input() todo: Todo = { text: '', done: false};
+  
+  updateStatus(): void {
+    this.todo.done = !this.todo.done;
+  }
 }
