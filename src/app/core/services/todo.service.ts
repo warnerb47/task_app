2199d311e41325a711ea8551a633bb73e2f83f11
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-20 12:27:31 +0000

    feat: add CoreTodoService

---
+++ src/app/core/services/todo.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from '../interfaces/todo';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoreTodoService {
+
+  constructor(private _httpClient: HttpClient) { }
+
+  fetchTodos(): Observable<Todo[]> {
+    const endpoint = '/todos';
+    return this._httpClient.get<Todo[]>(
+      environment.url + endpoint
+    );
+  }
+
+
+  fetchTodo(id: string): Observable<Todo[]> {
+    const endpoint = '/todos';
+    return this._httpClient.get<Todo[]>(
+      environment.url + endpoint + id
+    );
+  }
+
+  postTodo(payload: Todo): Observable<Todo> {
+    const endpoint = '/todos';
+    return this._httpClient.post<Todo>(
+      environment.url + endpoint,
+      payload,
+    );
+  }
+}
