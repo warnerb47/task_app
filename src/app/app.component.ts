ab71e3cc5a4cc8c3f4f69be4f9144d5f0dca1ebc
warnerb47 <taphagaye2001.mg@gmail.com>
2024-01-20 11:39:43 +0000

    feat: init components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskComponent } from './components/task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TaskFormComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
